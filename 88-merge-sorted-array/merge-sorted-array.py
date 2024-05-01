c701407df10c6dc32ebea8e5428c6f8fdd688aa1
Siddarth Satish <120223529+Sidd-satish12@users.noreply.github.com>
2024-05-01 18:58:17 -0400

    Time: 41 ms (48.42%) | Memory: 16.6 MB (78.17%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,7 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for index in range(0,n):
-            nums1[m + index] = nums2[index]
-        nums1.sort()
-        
+
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
